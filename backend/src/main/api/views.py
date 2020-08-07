d95168148228c770a1c85469e9186493fd898c8c
therealsharath <palathingal@gatech.edu>
2020-08-07 03:44:50 -0400

    Added recorder to frontend

--- backend/src/main/api/views.py
+++ backend/src/main/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListAPIView
 from main.models import Question, Lecture
 from .serializers import QuestionSerializer, LectureSerializer
 from . import answer
+from . import speech
 
 class QuestionListView(ListAPIView):
     queryset = Question.objects.all()
@@ -15,24 +16,19 @@ class LectureListView(ListAPIView):
 
 class AnswerListView(ListAPIView):
     questions = []
-    print(Question.objects.filter(confidence_score=1))
-    for question in Question.objects.filter(confidence_score=1):
+    for question in Question.objects.filter(confidence_score=0):
         questions.append(question.user_question)
     
-    answers = answer.predict(
-        """🤗 Magnesia and magnesium ribbon is heated. 
-        It starts burning or undergoes combustion engine revving bonds. 
-        It combines with oxygen to form magnesium oxide and liberals heat and light.""", questions
-    )
+    if questions != []:
+        sample_speech = speech.sample_recognize("gs://cloud-samples-tests/speech/brooklyn.flac")
+        print(sample_speech)
+        
+        answers = answer.predict(sample_speech, questions)
+        for question, answer in zip(questions, answers):
+            to_change = Question.objects.filter(user_question=question)[0]
+            to_change.model_answer=answer
+            to_change.save()
 
-    result = []
-    for question, answer in zip(questions, answers):
-        to_change = Question.objects.filter(user_question=question)[0]
-        to_change.model_answer=answer
-        to_change.save()
-        result.append((question, answer))
-
-    print(result)
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
